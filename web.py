2f18e9e2573714d033e3e7e3f6a15fefaea328c2
fkaimphilat <super.igoryan2000@gmail.com>
2022-12-23 20:32:40 +0300

    new tabs added

--- web.py
+++ web.py
@@ -8,8 +8,13 @@ app = Flask(__name__)
 #REST_URL = os.environ.get("REST_URL", "http://localhost/")
 
 @app.route('/')
+def startup_base():
+    return render_template('home.html.j2')
+
+@app.route('/about')
 def startup_home():
-    return render_template('index.html.j2')
+    return render_template('about.html.j2')
+
 
 if __name__ == '__main__':
     app.run(debug = True, host = '0.0.0.0')  
